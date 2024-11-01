55cc81b3b67663e591b63a204e5c3bb4dc007aff
chsaibabu <chinthasaibabu@gmail.com>
2024-11-01 21:08:37 +0530

    011124 2108

---
+++ shell-script/13-logs.sh
@@ -0,0 +1,32 @@
+#!bin/bash
+
+ID=$(id -u)
+
+echo "Script name $0"
+
+VALIDATE(){
+  if [ $1 -ne 0 ]
+  then
+      echo "Error $2 is failed"
+      exit 1
+  else 
+      echo "$2 is success"    
+  fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo "Error Please Run Script as Root Access"
+    exit 1
+else
+    echo "You are root user"    
+fi
+
+yum install mysql -y
+
+VALIDATE $? "Installing MYSQL"
+
+yum install git -y
+
+VALIDATE $? "Installing GIT"
+    
