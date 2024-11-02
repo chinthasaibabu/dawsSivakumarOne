cbccaa10b743e6838a7d4ec66b46b2f73212a2ee
chsaibabu <chinthasaibabu@gmail.com>
2024-11-02 10:09:40 +0530

    021124 1009

--- shell-script/15-installpackages.sh
+++ shell-script/15-installpackages.sh
@@ -6,6 +6,17 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script Started at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+  if [ $1 -ne 0 ]
+  then 
+      echo -e "$2 ... $R Failed $N"
+}
+
 if [ $ID -ne 0 ]
 then 
     echo -e "$R ERROR - Run Script as Root User $N"
@@ -14,4 +25,17 @@ else
     echo "You are Root User"
 fi
 
-echo "All Arguments Passed $@"
+#echo "All Arguments Passed $@"
+
+#git mysql postfix net-tools
+for package in $@
+do
+  yum list installed $package &>> $LOGFILE
+  if [ $? -ne 0 ]
+  then 
+      yum install $package -y &>> $LOGFILE
+      VALIDATE $? "Installation of $package"
+  else
+      echo -e "$package is already installed ... $Y Skipping $N"
+  fi        
+done
