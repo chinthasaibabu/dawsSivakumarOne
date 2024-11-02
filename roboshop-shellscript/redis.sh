6ac3f2617bf2f6a8db54a7373dbfa109148ca9ae
chsaibabu <chinthasaibabu@gmail.com>
2024-11-02 15:01:38 +0530

    021124 1501

--- roboshop-shellscript/redis.sh
+++ roboshop-shellscript/redis.sh
@@ -46,7 +46,7 @@ dnf install redis -y
 
 VALIDATE $? "Installing Redis"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf 
 
 VALIDATE $? "Allwoing Remote connections"
 
