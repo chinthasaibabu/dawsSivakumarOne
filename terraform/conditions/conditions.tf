b37dd2b6e217b02c23390f84c2d5dd353449cfab
chsaibabu <chinthasaibabu@gmail.com>
2024-11-07 21:36:23 +0530

    071124 2136

---
+++ terraform/conditions/conditions.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "web" {
+  ami           = var.ami_id #devops-practice
+  instance_type = var.instance_name == "MongoDB" ? "t3.small" : "t2.micro"
+  
+  #security_groups = [ aws_security_group.roboshop-all.id ]
+  #vpc_security_group_ids = [ aws_security_group.roboshop-all.id ]
+
+  #tags = var.tags
+}
